206d2037bce1156b28ed8b84320bffd4124715bd
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-08-14 18:08:03 +0100

    add responsiveness to navbarr

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,24 @@ myLogin.addEventListener("click", (e) => {
     })
 
 })
+
+//* Menu Button
+
+
+let menuBtn = document.querySelector(".menu-icon")
+let navigation = document.querySelector(".navigation")
+let menu = document.querySelector(".menu-icon")
+let closeBtn = document.querySelector("#close")
+
+
+menuBtn.addEventListener("click", () => {
+    navigation.style.display = "flex"
+    menu.style.display = "none"
+    closeBtn.style.display = "block"
+})
+
+closeBtn.addEventListener("click", () => {
+    navigation.style.display = "none"
+    menu.style.display = "block"
+    closeBtn.style.display = "none"
+})
